b50446bfd019e3924989fc60a3f83dd47b7123b9
nhuyeumeo2811-del <nhuyeumeo2811@amail.com>
2026-03-16 09:41:57 +0700

    Add multiple files

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Welcome from './welcome'
+import Loig from './components'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div className="card-title">
-        <Welcome name="Nguyễn Ngọc Quỳnh Như"/>
-        <p>Lớp: 26.SP.IT3</p>
-        <p>Sở thích: Ngủ</p>
-      </div>
-    </>
+    <div >
+      <Loig />
+    </div>
   )
 }
 
