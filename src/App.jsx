bdca15f723bfe6799390e37b424d9c42730fb920
nhuyeumeo2811-del <nhuyeumeo2811@amail.com>
2026-03-16 09:43:19 +0700

    Add multiple files

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Loig from './components'
-
+import Login from "./components/Login";
 function App() {
 
   return (
